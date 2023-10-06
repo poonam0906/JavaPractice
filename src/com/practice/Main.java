3a8c18c28182596efabca28eae7d317a070eed2f
poonam0906 <poonam.ashtake@gmail.com>
2023-10-06 22:51:25 +0530

    added hello world program

---
+++ src/com/practice/Main.java
@@ -0,0 +1,19 @@
+package com.practice;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (float i = 1; i <= 5; i++) {
+            double s = i + 0.5;
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("s = " + s);
+        }
+    }
+}
